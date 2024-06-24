5bdcf8850251382754688a4bb67f699348d643f1
bunabloom <kimrindev@gmail.com>
2024-06-24 20:05:16 +0900

    ㅁ

--- CalculatorStoryboard/ViewController.swift
+++ CalculatorStoryboard/ViewController.swift
@@ -12,19 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet var numButton: [UIButton]!
     
-    var resultnum = 0
-    var num1: Int = 0
-    var num2: Int = 0
     var temp: String = "0"
-    
-    func buttonColor() {
-        for i in numButton {
-            i.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
-            
-        }
-    }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,70 +28,40 @@ class ViewController: UIViewController {
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         let num = sender.currentTitle!
-        // 조건 등록 연산이 중복되지 않도록 // 십이상숫자가될경우 앞에 0을 지우는것
-        switch Int(num)! {
-        case 1...9: temp.append(num)
-        default :
-        temp.append(<#T##other: String##String#>)
-        }
-        
-        
         temp.append(num)
+        if temp[temp.startIndex] == "0" && temp.count > 2 {
+            temp.removeFirst()
+        }
         sendToLabel()
     }
     
-    func invalid(op: String) -> String {
-        let temp2: Character = Character(temp.index(temp.endIndex, offsetBy: -1))
-        if temp2 == "+" || temp2 == "-" || temp2 == "/" || temp2 == "*" {
-            
-        }
+//    func invalid(op: String) -> String {
+//        let temp2: Character = Character(temp.index(temp.endIndex, offsetBy: -1))
+//        if temp2 == "+" || temp2 == "-" || temp2 == "/" || temp2 == "*" {
+//            
+//        }
         
-    }
+    //}
     
     @IBAction func opButPressed(_ sender: UIButton) {
         let operation = sender.currentTitle!
         temp.append(operation)
         sendToLabel()
-        
     }
     
-    
-    
-    
     @IBAction func reset(_ sender: UIButton) {
-        
         temp = "0"
         sendToLabel()
     }
     
     @IBAction func equalButton(_ sender: UIButton) {
         temp = String(calculate(expression: temp)!)
-        
         sendToLabel()
     }
     
-    
-    
-    
-    
-    
     func sendToLabel(){ //레이블에 지속전달 디스플레이를 위한
         numberLabel.text = temp
-            
-            
-        
-        
-        
     }
-    //operator 눌렸을때 -> num1 전달 연산자 전달 연산자 누른순간 계산
-    func invalid(num: String, operator: String) -> Int{
-        
-        let temp2 = Int(num)!
-        
-        return temp2
-        
-    }
-    
     
     func calculate(expression: String) -> Int? {
         let expression = NSExpression(format: expression)
@@ -114,5 +72,13 @@ class ViewController: UIViewController {
         }
     }
     
+    func buttonColor() {
+        for i in numButton {
+            i.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+            
+        }
+    }
+    
 }
+
 
